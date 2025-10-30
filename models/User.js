3f93ced1e3a79569f30446a2e6f636f189becb82
Setulal Chowhan <setulalchowhan@gmail.com>
2025-10-30 17:09:05 +0600

    update default country in user schema to Bangladesh

--- models/User.js
+++ models/User.js
@@ -27,7 +27,7 @@ const userSchema = new mongoose.Schema(
       zipCode: String,
       country: {
         type: String,
-        default: "India",
+        default: "Bangladesh",
       },
     },
     role: {
